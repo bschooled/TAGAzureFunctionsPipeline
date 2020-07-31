bbad9c5c50303090317262c0e91a5560ebfa33e2
Martyn Coupland <martyn.coupland@ensono.com>
2020-07-31 15:12:06 +0100

    Initial commit

---
+++ HelloWorldFunction/HelloWorld.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldFunction
+{
+    public static class HelloWorld
+    {
+        [FunctionName("HelloWorld")]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation("Hello World");
+        }
+    }
+}
